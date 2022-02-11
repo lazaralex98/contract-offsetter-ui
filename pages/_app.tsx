8b1d55d4fd7fd152add8f7ef71c46f41770401ad
lazaralex98 <alex2012ro@gmail.com>
2022-02-11 15:51:43 +0200

    added react-toastify to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ToastContainer, ToastOptions } from "react-toastify";
+
+const toastOptions: ToastOptions = {
+  position: "bottom-right",
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
