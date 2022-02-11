6c885b779015ad54b5fafafe4f66c86d4a9ada44
lazaralex98 <alex2012ro@gmail.com>
2022-02-11 20:04:03 +0200

    ready to fill dashboard with content

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -81,9 +81,7 @@ const Dashboard: NextPage = ({
           <main>
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
               {/* Replace with your content */}
-              <div className="px-4 py-8 sm:px-0">
-                <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-              </div>
+
               {/* /End replace */}
             </div>
           </main>
