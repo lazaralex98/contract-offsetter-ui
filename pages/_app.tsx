6ca0ef3bb0e2aedd5c32d4990e0e71741f51945b
lazaralex98 <alex2012ro@gmail.com>
2022-02-11 15:52:34 +0200

    moved toast options to own file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,6 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastContainer, ToastOptions } from "react-toastify";
 
-const toastOptions: ToastOptions = {
-  position: "bottom-right",
-  autoClose: 5000,
-  hideProgressBar: false,
-  closeOnClick: true,
-  pauseOnHover: true,
-  draggable: true,
-  progress: undefined,
-};
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
---
+++ utils/toastOptions.ts
@@ -0,0 +1,13 @@
+import { ToastOptions } from "react-toastify";
+
+const toastOptions: ToastOptions = {
+  position: "bottom-right",
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+};
+
+export default toastOptions;
