7f1999338f56c56b7fd186de4a14b47d7bdf27bb
lazaralex98 <alex2012ro@gmail.com>
2022-02-11 21:01:31 +0200

    fixed a double btn issue

--- components/AppNavbar.tsx
+++ components/AppNavbar.tsx
@@ -70,12 +70,9 @@ const AppNavbar = ({
                   <div>
                     <Menu.Button className="max-w-xs bg-white flex items-center text-sm">
                       <span className="sr-only">Open user menu</span>
-                      <button
-                        type="button"
-                        className="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                      >
+                      <span className="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         {wallet}
-                      </button>
+                      </span>
                     </Menu.Button>
                   </div>
                   <Transition
