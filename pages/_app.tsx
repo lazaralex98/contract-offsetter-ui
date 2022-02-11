286c0958158acd28bfc0f99b956bb91b26fea7bd
lazaralex98 <alex2012ro@gmail.com>
2022-02-11 15:52:41 +0200

    moved toast options to own file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ToastContainer, ToastOptions } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
