e1255beca04106b9b2d155b14fca03e8c9f346c1
lazaralex98 <alex2012ro@gmail.com>
2022-02-12 18:33:56 +0200

    copy edit

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -244,7 +244,7 @@ const Dashboard: NextPage = ({
                       </div>
                       <div className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
                         <dt className="text-sm font-medium text-gray-500 truncate">
-                          Overall Emmissions
+                          Overall CO2 Emmissions
                         </dt>
                         <dd className="mt-1 text-3xl font-semibold text-gray-900">
                           {overallEmmissions} kg
