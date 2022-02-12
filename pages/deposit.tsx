9b5090492f7c6c99d19cf1b14d3722b73c19b34f
lazaralex98 <alex2012ro@gmail.com>
2022-02-12 22:07:42 +0200

    render balances

--- pages/deposit.tsx
+++ pages/deposit.tsx
@@ -13,6 +13,7 @@ import { BaseCarbonTonne } from "../contract-utils/BaseCarbonTonne";
 import { ToucanCarbonOffsets } from "../contract-utils/ToucanCarbonOffsets";
 import { Loader } from "../components/Loader";
 import ConnectWalletAlert from "../components/ConnectWalletAlert";
+import Link from "next/link";
 
 interface ifcDepositProps {
   wallet: string;
@@ -336,6 +337,82 @@ const Deposit: NextPage = ({
                   </div>
                 </div>
               </form>
+              {balances ? (
+                <>
+                  <h2 className="text-2xl font-bold leading-tight text-gray-900">
+                    Balances
+                  </h2>
+                  <div className="mt-5 flex flex-col">
+                    <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                      <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                          <table className="min-w-full divide-y divide-gray-200">
+                            <thead className="bg-gray-50">
+                              <tr>
+                                <th
+                                  scope="col"
+                                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                >
+                                  Address
+                                </th>
+                                <th
+                                  scope="col"
+                                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                >
+                                  Symbol
+                                </th>
+                                <th
+                                  scope="col"
+                                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                >
+                                  Name
+                                </th>
+                                <th
+                                  scope="col"
+                                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                >
+                                  Balance
+                                </th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {balances.map((token, index) => (
+                                <tr
+                                  key={token.address}
+                                  className={
+                                    index % 2 === 0 ? "bg-white" : "bg-gray-50"
+                                  }
+                                >
+                                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                    <Link
+                                      href={`https://mumbai.polygonscan.com/address/${token.address}`}
+                                    >
+                                      <a className="text-indigo-600 hover:text-indigo-900">
+                                        {token.address.substring(0, 15) + "..."}
+                                      </a>
+                                    </Link>
+                                  </td>
+                                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    {token.symbol}
+                                  </td>
+                                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    {token.name}
+                                  </td>
+                                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    {token.balance}
+                                  </td>
+                                </tr>
+                              ))}
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                ""
+              )}
               {/* /End replace */}
             </div>
           </main>
