273f24b38492d2548d463fd89e414eb267ca2426
lazaralex98 <alex2012ro@gmail.com>
2022-02-12 18:33:13 +0200

    boilerplate for offseting UI

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -178,6 +178,11 @@ const Dashboard: NextPage = ({
     setOverallEmmissions(overallFootprint);
   };
 
+  const offsetAll = async () => {
+    // TODO offset all transactions
+    console.log("OFFSET");
+  };
+
   return (
     <>
       <Head>
@@ -190,11 +195,34 @@ const Dashboard: NextPage = ({
           navigation={navigation}
         />
         <div className="py-10">
-          <header>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <header className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 md:flex md:items-center md:justify-between">
+            <div className="flex-1 min-w-0">
               <h1 className="text-3xl font-bold leading-tight text-gray-900">
                 Dashboard
               </h1>
+            </div>
+            <div className="mt-4 flex md:mt-0 md:ml-4">
+              <Link href="/offset/">
+                <a
+                  type="button"
+                  className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Want to offset another address?
+                </a>
+              </Link>
+              {transactions ? (
+                <button
+                  onClick={() => {
+                    offsetAll();
+                  }}
+                  type="button"
+                  className="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Offset All
+                </button>
+              ) : (
+                ""
+              )}
             </div>
           </header>
           <main>
