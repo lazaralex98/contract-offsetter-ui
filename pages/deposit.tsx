9d4e13ebe41b2281f806828fbc03c64edb8b030c
lazaralex98 <alex2012ro@gmail.com>
2022-02-13 11:00:25 +0200

    separated fetchDepositableTokenTypes function

--- pages/deposit.tsx
+++ pages/deposit.tsx
@@ -14,6 +14,7 @@ import { ToucanCarbonOffsets } from "../contract-utils/ToucanCarbonOffsets";
 import { Loader } from "../components/Loader";
 import ConnectWalletAlert from "../components/ConnectWalletAlert";
 import Link from "next/link";
+import fetchDepositableTokenTypes from "../utils/fetchDepositableTokenTypes";
 
 interface ifcDepositProps {
   wallet: string;
@@ -125,23 +126,6 @@ const Deposit: NextPage = ({
       setLoading(false);
       fetchBalances();
     }
-  };
-
-  const fetchDepositableTokenTypes = async () => {
-    const response = await fetch(`/api/getAllTCO2Types`);
-    const data: any = await response.json();
-
-    // preparing tokens to check/display in an easily formattable way
-    const tokensToCheck = [
-      ...data,
-      {
-        __typename: "BaseCarbonTonne",
-        address: process.env.NEXT_PUBLIC_BCT_ADDRESS_MUMBAI,
-        name: "Toucan Protocol: Base Carbon Tonne",
-        symbol: "BCT",
-      },
-    ];
-    return tokensToCheck;
   };
 
   const fetchBalances = async () => {
