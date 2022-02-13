94fcd6e601f5195a21ab40e26094e9657f440dd3
lazaralex98 <alex2012ro@gmail.com>
2022-02-13 22:38:02 +0200

    made a form to send you to the panel of the contract you want to offset

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -299,7 +299,7 @@ const Dashboard: NextPage = ({
                   type="button"
                   className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  Want to offset another address?
+                  Want to offset another address' transactions?
                 </a>
               </Link>
               {transactions ? (
