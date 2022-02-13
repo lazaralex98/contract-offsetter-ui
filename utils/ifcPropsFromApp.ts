3cea1732bb01a1f64e7647852b8ed5627878ffbf
lazaralex98 <alex2012ro@gmail.com>
2022-02-13 12:35:19 +0200

    more separation

---
+++ utils/ifcPropsFromApp.ts
@@ -0,0 +1,9 @@
+interface ifcPropsFromApp {
+  wallet: string;
+  connectWallet: Function;
+  disconnectWallet: Function;
+  loading: boolean;
+  setLoading: Function;
+}
+
+export default ifcPropsFromApp;
