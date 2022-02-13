3aa76a65ea292337c6c162c577f1bf5522e8d9f8
lazaralex98 <alex2012ro@gmail.com>
2022-02-13 16:55:33 +0200

    todo

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -76,7 +76,7 @@ const Dashboard: NextPage = ({
   /**
    * fetches an array of transactions for the given address
    * @param address address of user/contract to fetch transactions for
-   * @issue the API endpoint returns a maximum of 10,000 records only
+   * @issue TODO NOTE WARNING the API endpoint returns a maximum of 10,000 records only
    */
   const fetchTransactionsOfAddress = async (address: string) => {
     try {
