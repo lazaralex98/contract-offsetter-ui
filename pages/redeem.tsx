47bfca1275b2989cfab4338c513ad9c6aa43b0a0
lazaralex98 <alex2012ro@gmail.com>
2022-02-13 12:26:59 +0200

    redeem page boilerplate, but I have a very weird error

--- pages/redeem.tsx
+++ pages/redeem.tsx
@@ -1,3 +1,4 @@
+import { ethers } from "ethers";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useEffect, useState } from "react";
@@ -10,6 +11,9 @@ import fetchBalances from "../utils/fetchBalances";
 import fetchDepositableTokenTypes from "../utils/fetchDepositableTokenTypes";
 import ifcBalance from "../utils/ifcBalance";
 import toastOptions from "../utils/toastOptions";
+import * as coAbi from "../contract-utils/ContractOffsetter.json";
+import * as bctAbi from "../contract-utils/BaseCarbonTonne.json";
+import * as tco2Abi from "../contract-utils/ToucanCarbonOffsets.json";
 
 interface ifcRedeemProps {
   wallet: string;
@@ -19,12 +23,14 @@ interface ifcRedeemProps {
 }
 // TODO make page
 // @ts-ignore some type props BS i don't have the time to look into right now
-const Redeem: NextPage = ({
+const Redeem: NextPage = async ({
   wallet,
   connectWallet,
   loading,
   setLoading,
 }: ifcRedeemProps) => {
+  return "x";
+
   const navigation = [
     { name: "Dashboard", href: "/dashboard", current: false },
     { name: "Deposit", href: "/deposit", current: false },
@@ -38,6 +44,12 @@ const Redeem: NextPage = ({
     },
     { name: "Disconnect", href: "/disconnect" },
   ];
+
+  // these are for the form
+  const [amount, setAmount] = useState<string>("1.0");
+  const [token, setToken] = useState<string>(
+    "(await fetchDepositableTokenTypes())[0].address"
+  );
 
   // this is for stats
   const [balances, setBalances] = useState<ifcBalance[] | null>(null);
@@ -77,6 +89,66 @@ const Redeem: NextPage = ({
     }
   };
 
+  const handleRedeemal = async () => {
+    try {
+      if (!wallet) {
+        throw new Error("Connect your wallet first.");
+      }
+      setLoading(true);
+
+      // @ts-ignore
+      const { ethereum } = window;
+      if (!ethereum) {
+        throw new Error("You need Metamask.");
+      }
+
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+
+      // get portal to ContractOffsetter
+      // @ts-ignore
+      const co: ContractOffsetter = new ethers.Contract(
+        process.env.NEXT_PUBLIC_CONTRACT_OFFSETTER_ADDRESS_MUMBAI || "",
+        coAbi.abi,
+        signer
+      );
+
+      // get portal to the token the user wants deposited (BCT or TCO2 for now)
+      const tokenPortal = new ethers.Contract(
+        token,
+        token == process.env.NEXT_PUBLIC_BCT_ADDRESS_MUMBAI
+          ? bctAbi.abi
+          : tco2Abi.abi,
+        signer
+      );
+
+      // token needs to approve ContractOffsetter first
+      await (
+        await tokenPortal.approve(co.address, ethers.utils.parseEther(amount))
+      ).wait();
+
+      // we then deposit the amount of TCO2/BCT into the ContractOffsetter
+      const depositTxn = await co.deposit(
+        token,
+        ethers.utils.parseEther(amount),
+        {
+          gasLimit: 1200000,
+        }
+      );
+      await depositTxn.wait();
+
+      console.log("deposit hash", depositTxn.hash);
+
+      toast(`You deposited ${amount} TCO2s`, toastOptions);
+    } catch (error: any) {
+      console.error("error when depositing", error);
+      toast.error(error.message, toastOptions);
+    } finally {
+      setLoading(false);
+      storeBalances();
+    }
+  };
+
   useEffect(() => {
     storeBalances();
   }, [wallet]);
@@ -112,7 +184,94 @@ const Redeem: NextPage = ({
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
               {/* Replace with your content */}
               <div className="px-4 py-8 sm:px-0">
-                <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    handleRedeemal();
+                  }}
+                  className="space-y-8 divide-y divide-gray-200"
+                >
+                  <div className="space-y-8 divide-y divide-gray-200">
+                    <div className="pt-8">
+                      <div>
+                        <p className="mt-1 text-sm text-gray-500">
+                          Choose what TCO2 you want to redeem your BCT for and
+                          how much of it you want redeemed.
+                        </p>
+                      </div>
+                      <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+                        {/* say amount to deposit */}
+                        <div className="sm:col-span-4">
+                          <label
+                            htmlFor="email"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Amount to redeem
+                          </label>
+                          <div className="mt-1">
+                            <input
+                              onChange={(e) => {
+                                setAmount(e.target.value);
+                              }}
+                              value={amount}
+                              id="amount"
+                              name="amount"
+                              type="text"
+                              autoComplete="amount"
+                              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                            />
+                          </div>
+                        </div>
+
+                        {/* select coin to receive */}
+                        <div className="sm:col-span-3">
+                          <label
+                            htmlFor="country"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            What do you want to get?
+                          </label>
+                          <div className="mt-1">
+                            <select
+                              onChange={(e) => {
+                                setToken(e.target.value);
+                              }}
+                              id="token"
+                              name="token"
+                              autoComplete="token"
+                              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                            >
+                              {balances
+                                ?.filter((token) => {
+                                  if (token.symbol == "BCT") {
+                                    return token;
+                                  }
+                                })
+                                .map((token) => {
+                                  return (
+                                    <option value={token.address}>
+                                      {token.symbol}
+                                    </option>
+                                  );
+                                })}
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="pt-5">
+                    <div className="flex justify-end">
+                      <button
+                        type="submit"
+                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      >
+                        Deposit
+                      </button>
+                    </div>
+                  </div>
+                </form>
                 {balances ? <BalancesTable balances={balances} /> : ""}
               </div>
               {/* /End replace */}
