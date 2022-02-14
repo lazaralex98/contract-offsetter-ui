9f72af3cb12e745a0cf13f0ab521d9c46c351e9d
lazaralex98 <alex2012ro@gmail.com>
2022-02-14 12:17:34 +0200

    separated transsactions table

---
+++ components/TransactionsTable.tsx
@@ -0,0 +1,93 @@
+import Link from "next/link";
+import { ifcFormattedTransaction } from "../utils/ifcTransaction";
+
+const TransactionsTable = ({
+  transactions,
+}: {
+  transactions: ifcFormattedTransaction[];
+}) => {
+  return (
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Hash
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Gas Used
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Nonce
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Transaction Status
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Offset Status
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {transactions.map((transaction, index) => (
+                  <tr
+                    key={transaction.hash}
+                    className={index % 2 === 0 ? "bg-white" : "bg-gray-50"}
+                  >
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      <Link
+                        href={`https://mumbai.polygonscan.com/tx/${transaction.hash}`}
+                      >
+                        <a className="text-indigo-600 hover:text-indigo-900">
+                          {transaction.hash.substring(0, 15) + "..."}
+                        </a>
+                      </Link>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {transaction.gasUsed}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {transaction.nonce}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {transaction.transactionStatus}
+                    </td>
+                    <td
+                      className={`px-6 py-4 whitespace-nowrap text-sm text-gray-500 ${
+                        transaction.offsetStatus
+                          ? "text-green-600"
+                          : "text-red-600 underline"
+                      }`}
+                    >
+                      {String(transaction.offsetStatus)}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TransactionsTable;
--- pages/offset/[address].tsx
+++ pages/offset/[address].tsx
@@ -14,6 +14,7 @@ import { Loader } from "../../components/Loader";
 import fetchOffsetStatus from "../../utils/fetchOffsetStatus";
 import fetchAndFormatTransactions from "../../utils/fetchAndFormatTransactions";
 import { ifcFormattedTransaction } from "../../utils/ifcTransaction";
+import TransactionsTable from "../../components/TransactionsTable";
 
 // @ts-ignore some type props BS i don't have the time to look into right now
 const Offset: NextPage = ({
@@ -309,89 +310,7 @@ const Offset: NextPage = ({
 
                 {/* show transactions table */}
                 {transactions ? (
-                  <div className="flex flex-col">
-                    <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                      <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                          <table className="min-w-full divide-y divide-gray-200">
-                            <thead className="bg-gray-50">
-                              <tr>
-                                <th
-                                  scope="col"
-                                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                >
-                                  Hash
-                                </th>
-                                <th
-                                  scope="col"
-                                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                >
-                                  Gas Used
-                                </th>
-                                <th
-                                  scope="col"
-                                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                >
-                                  Nonce
-                                </th>
-                                <th
-                                  scope="col"
-                                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                >
-                                  Transaction Status
-                                </th>
-                                <th
-                                  scope="col"
-                                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                >
-                                  Offset Status
-                                </th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              {transactions.map((transaction, index) => (
-                                <tr
-                                  key={transaction.hash}
-                                  className={
-                                    index % 2 === 0 ? "bg-white" : "bg-gray-50"
-                                  }
-                                >
-                                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                    <Link
-                                      href={`https://mumbai.polygonscan.com/tx/${transaction.hash}`}
-                                    >
-                                      <a className="text-indigo-600 hover:text-indigo-900">
-                                        {transaction.hash.substring(0, 15) +
-                                          "..."}
-                                      </a>
-                                    </Link>
-                                  </td>
-                                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    {transaction.gasUsed}
-                                  </td>
-                                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    {transaction.nonce}
-                                  </td>
-                                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    {transaction.transactionStatus}
-                                  </td>
-                                  <td
-                                    className={`px-6 py-4 whitespace-nowrap text-sm text-gray-500 ${
-                                      transaction.offsetStatus
-                                        ? "text-green-600"
-                                        : "text-red-600 underline"
-                                    }`}
-                                  >
-                                    {String(transaction.offsetStatus)}
-                                  </td>
-                                </tr>
-                              ))}
-                            </tbody>
-                          </table>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  <TransactionsTable transactions={transactions} />
                 ) : (
                   <div className="h-96 flex justify-around items-center">
                     <button
