0751708e8d3b33424f8865f0e0c0ba97edcf9cf8
lazaralex98 <alex2012ro@gmail.com>
2022-02-14 16:22:43 +0200

    calling the fetch transactions on wrong address - fixed

--- pages/offset/[address].tsx
+++ pages/offset/[address].tsx
@@ -315,7 +315,7 @@ const Offset: NextPage = ({
                   <div className="h-96 flex justify-around items-center">
                     <button
                       onClick={async () => {
-                        await getAndStoreTransactions(wallet);
+                        await getAndStoreTransactions(addressFromQuery);
                       }}
                       type="button"
                       className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
