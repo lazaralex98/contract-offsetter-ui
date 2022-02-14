2916902b24e12db677d368a14cce01a0c15129fe
lazaralex98 <alex2012ro@gmail.com>
2022-02-14 12:22:39 +0200

    bigget 'load transactions' btn

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -308,11 +308,11 @@ const Dashboard: NextPage = ({
                 ) : (
                   <div className="h-96 flex justify-around items-center">
                     <button
-                      type="button"
-                      className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                       onClick={async () => {
                         await getAndStoreTransactions(wallet);
                       }}
+                      type="button"
+                      className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                       Load My Transactions
                     </button>
--- pages/offset/[address].tsx
+++ pages/offset/[address].tsx
@@ -314,11 +314,11 @@ const Offset: NextPage = ({
                 ) : (
                   <div className="h-96 flex justify-around items-center">
                     <button
-                      type="button"
-                      className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                       onClick={async () => {
-                        await getAndStoreTransactions(addressFromQuery);
+                        await getAndStoreTransactions(wallet);
                       }}
+                      type="button"
+                      className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                       Load Transactions of {addressFromQuery}
                     </button>
