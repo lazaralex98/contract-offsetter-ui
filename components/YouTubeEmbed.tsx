14394c47ae52ea8705aeed35ff728795f55047ab
lazaralex98 <alex2012ro@gmail.com>
2022-02-16 16:54:27 +0200

    added demo video

---
+++ components/YouTubeEmbed.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const YouTubeEmbed = ({ embedId }: { embedId: string }) => (
+  <div className="overflow-hidden pb-[480px] relative h-0">
+    <iframe
+      className="left-0 top-0 h-full w-full absolute"
+      width="853"
+      height="480"
+      src={`https://www.youtube.com/embed/${embedId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div>
+);
+
+export default YouTubeEmbed;
